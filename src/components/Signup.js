d14f0e0a7400ed2496ff1d29baa24805f5bceca4
hashi-02 <hassie0209@gmail.com>
2022-03-28 12:58:14 +0900

    signupform作成

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,39 @@
+import React from 'react';
+import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
-
 const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <>
+    <div>
       <h1>新規登録ページ</h1>
-
-      <div></div>
       <div>
         ログインは<Link to={`/login/`}>こちら</Link>
       </div>
       <div>
         <Link to={`/`}>ホームに戻る</Link>
       </div>
-    </>
+
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+      <div></div>
+    </div>
   );
 };
 
