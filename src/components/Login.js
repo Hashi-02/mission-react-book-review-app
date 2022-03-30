58c8c6fa3f2a5cb4e4f88eb7e5ad9cd310c363eb
hashi-02 <hassie0209@gmail.com>
2022-03-30 18:02:04 +0900

    バリデーション 軽く実装

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useFormik } from 'formik';
 import Axios from 'axios';
 import React from 'react';
+import * as Yup from 'yup';
 const url = 'https://api-for-missions-and-railways.herokuapp.com/signin';
 
 const Login = () => {
@@ -11,9 +12,14 @@ const Login = () => {
       email: '',
       password: '',
     },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+      password: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+    }),
     onSubmit: (values) => {
       console.log(JSON.stringify(values, null, 2));
-
       Axios.post(url, {
         email: formik.values.email,
         password: formik.values.password,
@@ -27,15 +33,15 @@ const Login = () => {
     },
   });
 
-  if (error)
-    return (
-      <div>
-        {`Error: ${error.message}`}
-        <div>
-          <Link to={`/`}>ホームに戻る</Link>
-        </div>
-      </div>
-    );
+  // if (error)
+  //   return (
+  //     <div>
+  //       {`Error: ${error.message}`}
+  //       <div>
+  //         <Link to={`/`}>ホームに戻る</Link>
+  //       </div>
+  //     </div>
+  //   );
 
   return (
     <>
@@ -43,6 +49,8 @@ const Login = () => {
       <div>
         新規登録は<Link to={`/signup/`}>こちら</Link>
       </div>
+      <div>{error && <p>{error.message}</p>}</div>
+
       <div>
         <Link to={`/`}>ホームに戻る</Link>
       </div>
@@ -54,16 +62,25 @@ const Login = () => {
             name="email"
             type="email"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+
           <label htmlFor="password">password</label>
           <input
             id="password"
             name="password"
             type="password"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.password}
           />
+          {formik.touched.password && formik.errors.password ? (
+            <div>{formik.errors.password}</div>
+          ) : null}
 
           <button type="submit">Submit</button>
         </form>
