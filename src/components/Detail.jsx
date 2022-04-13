62439155fe7ce5baff88fa707b20e721014bb531
hashi-02 <hassie0209@gmail.com>
2022-04-14 00:24:42 +0900

    DetailPageのLoading実装

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import LoadingSpinner from './LoadingSpinner';
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 const Detail = () => {
   const [review, setReview] = React.useState([]);
+  const [loading, setLoading] = React.useState('');
   const TOKEN = localStorage.getItem('token');
   const id = useParams();
   const url = 'https://api-for-missions-and-railways.herokuapp.com/books/';
@@ -19,6 +21,7 @@ const Detail = () => {
         console.log(res.data);
         const result = res.data;
         setReview(result);
+        setLoading('True');
       })
       .catch((res) => {
         console.log(id);
@@ -28,16 +31,22 @@ const Detail = () => {
 
   return (
     <div>
-      <p>id:{review.id}</p>
-      <p>タイトル:{review.title}</p>
-      <p>レビュー：{review.review}</p>
-      <p>詳細：{review.detail}</p>
-      <p>URL:{review.url}</p>
-      <p>
-        レビュワー:
-        {review.reviewer}
-      </p>
-      {review.isMine && <Link to={`/edit/${review.id}`}>編集する</Link>}
+      {loading ? (
+        <div>
+          <p>id:{review.id}</p>
+          <p>タイトル:{review.title}</p>
+          <p>レビュー：{review.review}</p>
+          <p>詳細：{review.detail}</p>
+          <p>URL:{review.url}</p>
+          <p>
+            レビュワー:
+            {review.reviewer}
+          </p>
+          {review.isMine && <Link to={`/edit/${review.id}`}>編集する</Link>}
+        </div>
+      ) : (
+        <LoadingSpinner />
+      )}
     </div>
   );
 };
---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="animate-spin h-8 w-8 bg-blue-300 rounded-xl"></div>
+    </div>
+  );
+};
+export default LoadingSpinner;
